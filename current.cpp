7ad1d49082abea76faeaa06b44d802a736457a43
Matvey Yushkin <62177479+GIBBS-Matvey@users.noreply.github.com>
2024-09-29 15:59:48 +0300

    Update current.cpp

--- current.cpp
+++ current.cpp
@@ -55,32 +55,36 @@ private:
     };
 
     std::vector<std::vector<Edge>> adj;
-    size_t vNumber;
+    size_t vertex_number;
+    
+    int get_hash_edge(int from, int to, int n) {
+        return from * n + to;
+    }
 
 
 public:
-    explicit Graph(size_t vNumber) : vNumber(vNumber), adj(vNumber) {}
+    explicit Graph(size_t vNumber) : vertex_number(vNumber), adj(vNumber) {}
 
     Graph(const std::vector<std::vector<int>>& matrix) {
 
     }
 
 
-    void addAdj(int from, int to) {
+    void add_edge(int from, int to) {
         adj[from].push_back({to});
     }
 
     template <class Visitor>
     void Dfs(Visitor vis, std::vector<std::string>& color, const std::vector<int>& callOrderV = std::vector<int>()) {
 
-        std::vector<int> callOrderVector(vNumber);
+        std::vector<int> callOrderVector(vertex_number);
 
         if (!callOrderV.empty()) {
             for (auto v : callOrderV) {
                 callOrderVector.emplace_back(v);
             }
         } else {
-            for (int v = 0; v < vNumber; ++v) {
+            for (int v = 0; v < vertex_number; ++v) {
                 callOrderVector.emplace_back(v);
             }
         }
@@ -112,10 +116,10 @@ public:
     }
 
     Graph getTransposed() const {
-        Graph trGraph(vNumber);
-        for (int from = 0; from < vNumber; ++from) {
+        Graph trGraph(vertex_number);
+        for (int from = 0; from < vertex_number; ++from) {
             for (auto& neiEdge : adj[from]) {
-                trGraph.addAdj(neiEdge.to, from);
+                trGraph.add_edge(neiEdge.to, from);
             }
         }
         return trGraph;
@@ -123,15 +127,15 @@ public:
 
     void FindStrongComponents() {
 
-        std::vector<std::string> color(vNumber);
-        std::vector<int> callVector(vNumber);
+        std::vector<std::string> color(vertex_number);
+        std::vector<int> callVector(vertex_number);
         TopSortVisitor sortVisitor(&callVector);
 
         Dfs(sortVisitor, color);
         std::reverse(callVector.begin(), callVector.end());
 
         int strongComponentNumber = 0;
-        std::vector<std::vector<int>> strongComponent(vNumber);
+        std::vector<std::vector<int>> strongComponent(vertex_number);
         StrongComponentVisitor sccVisitor(&strongComponent, &strongComponentNumber);
 
         getTransposed().Dfs(sccVisitor, color, callVector);
@@ -152,36 +156,42 @@ public:
     }
 
     Graph get_condensed() {
-        std::vector<std::string> color(vNumber);
-        std::vector<int> callVector(vNumber);
-        TopSortVisitor sortVisitor(&callVector);
+        std::vector<std::string> color(vertex_number);
+        std::vector<int> v_call(vertex_number);
+        TopSortVisitor sort_visitor(&v_call);
 
-        Dfs(sortVisitor, color);
-        std::reverse(callVector.begin(), callVector.end());
+        Dfs(sort_visitor, color);
+        std::reverse(v_call.begin(), v_call.end());
 
-        int strongComponentNumber = 0;
-        std::vector<std::vector<int>> scc(vNumber);
-        StrongComponentVisitor sccVisitor(&scc, &strongComponentNumber);
+        int scc_number = 0;
+        std::vector<std::vector<int>> scc(vertex_number);
+        StrongComponentVisitor scc_visitor(&scc, &scc_number);
 
-        getTransposed().Dfs(sccVisitor, color, callVector);
+        getTransposed().Dfs(scc_visitor, color, v_call);
 
-        std::vector<int> vSccNumber(vNumber);
+        std::vector<int> v_scc_number(vertex_number);
         for (int i = 0; i < scc.size(); ++i) {
             for (int vertex : scc[i]) {
-                vSccNumber[vertex] = i;
+                v_scc_number[vertex] = i;
             }
         }
 
-        Graph condensed_graph(strongComponentNumber);
-        for (int i = 0; i < strongComponentNumber; ++i) {
-            for (int j = 0; j < strongComponentNumber; ++j) {
-                if (i != j) {
-                    //todo
+        Graph condensed_graph(scc_number);
+        std::unordered_set<int> hash_table;
+        for (int i = 0; i < vertex_number; ++i) {
+            for (const Edge& edge : adj[i]) {
+                if (v_scc_number[i] != v_scc_number[edge.to]) { /// in different components
+                    
+                    int hashed_edge = get_hash_edge(v_scc_number[i], v_scc_number[edge.to], scc_number);
+                    if (hash_table.find(hashed_edge) == hash_table.end()) {
+                        hash_table.insert(hashed_edge);
+                        condensed_graph.add_edge(v_scc_number[i], v_scc_number[edge.to]);
+                    }
                 }
             }
         }
-
-
+        
+        return condensed_graph;
     }
 };
 
@@ -189,18 +199,18 @@ public:
 int main() {
     size_t vertex_number;
     std::cin >> vertex_number;
-
+    
     size_t edge_number;
     std::cin >> edge_number;
 
     Graph graph(vertex_number);
-
+    
     for (size_t i = 0; i < edge_number; ++i) {
         int from, to;
         std::cin >> from >> to;
-        graph.addAdj(from - 1, to - 1);
+        graph.add_edge(from - 1, to - 1);
     }
-
+    
     graph.FindStrongComponents();
 
     return 0;
