b55eaae04d9bbaca6dfc384956a7479aedb20129
Matvey Yushkin <62177479+GIBBS-Matvey@users.noreply.github.com>
2024-12-11 22:20:07 +0300

    Update Suffix_Tree.cpp

--- Algorithms and Data Structures/Texts/Suffix_Tree.cpp
+++ Algorithms and Data Structures/Texts/Suffix_Tree.cpp
@@ -24,18 +24,17 @@ private:
     std::string text;
     Node* root;
 
-    class Location {
-    private:
+    struct Location {
         Node* expl_parent;
         int delta_start;
         int delta_finish;
 
         const Node* const root;
         const std::string& text;
-    public:
 
         Location(Node* expl_parent, int delta_start, int delta_finish,
-                 const Node* const root, const std::string& text) : root(root), text(text) {}
+                 const Node* const root, const std::string& text) : expl_parent(expl_parent), root(root), text(text),
+                 delta_start(delta_start), delta_finish(delta_finish){}
 
         void move_next() {
             if (is_explicit()) {
@@ -72,12 +71,12 @@ private:
                 }
 
                 delta_start = cur_delta_start;
-                delta_finish = delta_start + cur_delta_len - 1; // надо ли в принципе хранить?
+                delta_finish = delta_start + cur_delta_len - 1; // надо ли в принципе хранить? upd: да
             }
         }
 
         bool is_root() const {
-            return expl_parent == root && delta_len() == 0;
+            return expl_parent == root && delta_start == -1;
         }
 
         bool is_leaf() const {
@@ -107,7 +106,67 @@ public:
         root->suffix_link = nullptr;
         root->map.insert({text[0], {0, 0, nullptr}});
         Location loc(root, 0, 0, root, text);
-        
+
+        for (int i = 0; i < text.size(); ++i) {
+            char new_sym = text[i];
+
+            while (loc.is_leaf()) {
+                loc.move_next();
+            }
+
+            // попали на первую нелистовую вершину
+
+            bool processed = false;
+
+            while (!processed) {
+
+
+                if (loc.is_explicit()) {    /// если находимся в явной вершине
+                    auto it = loc.expl_parent->map.find(new_sym);
+                    const Edge& edge = it->second;
+                    if (it == loc.expl_parent->map.end()) { // если не нашли new_sym в ветках
+                        Edge new_edge = {i, i, nullptr};    // отращиваем ребро
+                        loc.expl_parent->map.insert({new_sym, new_edge});
+                    } else { // если нашли new_sym в каком-то ребре, тогда начинаем с этой локации на след.итерации
+                        loc.delta_start = edge.start;
+                        loc.delta_finish = edge.start;
+                        processed = true;
+                    }
+                }
+
+                else {    /// если находимся в неявной вершине
+                    auto it = loc.expl_parent->map.find(text[loc.delta_start]);
+                    Edge& loc_edge = it->second;
+                    if (text[loc.delta_finish + 1] == new_sym) { // если new_sym лежит дальше по ребру, обновляем локацию для след.итерации
+                        if (loc.delta_len() < loc_edge.get_len()) { // локация остаётся неявной, продлеваем finish на 1
+                            ++loc.delta_finish;
+                        } else {
+                            loc.expl_parent = loc_edge.to; // локация становится явной
+                            loc.delta_start = -1;
+                            loc.delta_finish = -1;
+                        }
+                        processed = true;
+                    }
+                    else { // если new_sym нет дальше по ребру, тогда материализуем Node
+                        Node* new_node = new Node;
+                        if (loc_edge.get_len() > loc.delta_len()) {
+                            int after_index = loc.delta_finish + 1;
+                            new_node->map.insert({text[after_index], {after_index, loc_edge.finish, loc_edge.to}});
+                        }
+                        loc_edge.finish = loc.delta_finish; // +/- 1
+                        loc_edge.to = new_node;
+                        
+                        new_node->map.insert({new_sym, {i, i, nullptr}});
+                        //todo посчитать "руками" суффиксную ссылку для новой ноды и сохранить её в new_node->suffix_link
+                        
+                        loc.move_next();
+                    }
+                }
+            }
+
+
+        }
+
     }
 
 };
