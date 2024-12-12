91229de92eaec819c19689653a71c7b757db7562
Matvey Yushkin <62177479+GIBBS-Matvey@users.noreply.github.com>
2024-12-12 23:50:28 +0300

    Update Suffix_Tree.cpp

--- Algorithms and Data Structures/Texts/Suffix_Tree.cpp
+++ Algorithms and Data Structures/Texts/Suffix_Tree.cpp
@@ -6,7 +6,7 @@ private:
 
     struct Node;
     struct Location;
-    
+
     struct Edge {
         int start;
         int finish;
@@ -36,8 +36,8 @@ private:
         Location(Node* expl_parent, int delta_start, int delta_finish,
                  Node* root, const std::string& text) : expl_parent(expl_parent), root(root), text(text),
                  delta_start(delta_start), delta_finish(delta_finish) {}
-                 
-                 
+
+
         void move_suffix_link() {
             // гарантировано не вызываем от корня
 
@@ -53,7 +53,7 @@ private:
                 }
             }
         }
-        
+
         Location skip_count(Node* node, int start, int finish) {
             // гарантированно спускаемся в неявную нелистовую локацию, которая гарантированно отчитается от этого места
 
@@ -67,7 +67,7 @@ private:
                 cur_len -= cur_edge->get_len();
                 cur_edge = &(node->map.find(text[cur_start])->second);
             }
-            
+
             this->expl_parent = node;
             this->delta_start = cur_edge->start;
             this->delta_finish = cur_edge->start + cur_len - 1;
@@ -76,7 +76,7 @@ private:
         bool is_root() const {
             return expl_parent == root && delta_start == -1;
         }
-        
+
         bool is_leaf() const {
             if (!is_explicit()) {
                 auto it = expl_parent->map.find(text[delta_start]);
@@ -105,28 +105,34 @@ private:
         }
 
         void add_symbol(char symbol, int i) {
-            if (is_explicit()) {
-                expl_parent->map.insert({symbol, {i, -1, nullptr}});
-            }
-            else {
-                Edge& edge = expl_parent->map.find(text[delta_start])->second;
-
-                int new_edge_finish = (edge.to == nullptr) ? i - 1 : edge.finish;
-                int new_edge_start = delta_finish + 1;
-
-                Node* new_node = new Node;
-                new_node->map.insert({text[new_edge_start], {new_edge_start, new_edge_finish, edge.to}});
-                new_node->map.insert({symbol, {i, -1, nullptr}});
-
-                edge.finish = delta_finish;
-                edge.to = new_node;
-                //todo посчитать suffix_link для new_node
-            }
+            // гарантированно вызываем от явной локации, после которой нет symbol
+            expl_parent->map.insert({symbol, {i, -1, nullptr}});
         }
-        
+
+        Node* create_new_node(char symbol, int i, Node* prev_node) const {
+            // гарантированно вызываем от неявной локации, после которой нет symbol
+            Edge& edge = expl_parent->map.find(text[delta_start])->second;
+
+            int new_edge_finish = (edge.to == nullptr) ? i - 1 : edge.finish;
+            int new_edge_start = delta_finish + 1;
+
+            Node* new_node = new Node;
+            new_node->map.insert({text[new_edge_start], {new_edge_start, new_edge_finish, edge.to}});
+            new_node->map.insert({symbol, {i, -1, nullptr}});
+
+            edge.finish = delta_finish;
+            edge.to = new_node;
+
+            if (prev_node != nullptr) {
+                prev_node->suffix_link = new_node;
+            }
+            return new_node;
+            //todo посчитать suffix_link для new_node
+        }
+
         void move_down(char symbol) {
             // точно знаем, что symbol есть в продолжении текущей локации
-            
+
             const Edge& edge = expl_parent->map.find(symbol)->second;
             if (is_explicit()) {
                 if (edge.get_len() > 1 || (edge.get_len() == 1 && edge.to == nullptr)) {
@@ -170,16 +176,25 @@ public:
                 loc.move_suffix_link();
             }
 
-            while (!loc.has_symbol_after(new_sym)) { /// продуктивный блок - добавление новых рёбер
-                loc.add_symbol(new_sym, i);
+            // попадаем на первую нелистовую вершину
+            Node* new_node = nullptr;
+            while (!loc.has_symbol_after(new_sym)) {
 
+                if (loc.is_explicit()) {
+                    loc.add_symbol(new_sym, i);
+                } else {
+                    new_node = loc.create_new_node(new_sym, i, new_node);   /// продуктивный блок - добавление новых рёбер
+                }
+                
                 if (!loc.is_root()) {
                     loc.move_suffix_link();
                 } else {
-                    break; // частный случай: когда дошли loc-ом до корня, и у корня тоже нет new_sym
+                    break; // todo частный случай: когда дошли loc-ом до корня, и у корня тоже нет new_sym
                 }
             }
-
+            
+            // todo тут может остаться new_node, у которой не инициализирована suffix_link
+            
             loc.move_down(new_sym);
         }
     }
